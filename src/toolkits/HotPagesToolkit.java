dc4e5acd0acac51768bd83f38346084983dcdbb5
Shaoshing <shaoshing@me.com>
2014-02-06 17:08:52 -0500

    Add toolkits package.

---
+++ src/toolkits/HotPagesToolkit.java
@@ -0,0 +1,56 @@
+package toolkits;
+
+import com.wikipedia.iub.WikiCategory;
+import com.wikipedia.iub.WikiClient;
+import com.wikipedia.iub.WikiPage;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ * Created by shaoshing on 2/6/14.
+ */
+public class HotPagesToolkit {
+
+
+    static final int FIELD_INDEX_ID = 0;
+    static final int FIELD_INDEX_TITLE = 1;
+    public static void generateHotPagesTxt(String hotPagesFile){
+
+        try {
+            WikiClient wikiClient = new WikiClient("configs/wikipedia.property");
+            BufferedReader fileReader = new BufferedReader(new FileReader(hotPagesFile));
+            String line = null;
+            String hotPagesTxt = "";
+            PrintWriter writer = new PrintWriter(Main.OUTPUT_FOLDER+"/hot-pages.txt", "UTF-8");
+            while((line = fileReader.readLine()) != null){
+                String[] fields = line.split("\\|");
+                System.out.println(fields[FIELD_INDEX_TITLE]);
+                WikiPage page = wikiClient.findByPageId(Integer.parseInt(fields[FIELD_INDEX_ID]), WikiClient.LANGUAGE.ENGLISH);
+                if(page == null) continue;
+
+                line += "|" + page.titleTranslation;
+
+                ArrayList<WikiCategory>[] categoriesArray = new ArrayList[]{page.l1_categories, page.l2_categories};
+                for(ArrayList<WikiCategory> categories : categoriesArray){
+                    String categoryStr = "";
+                    for(int i = 0; i < categories.size(); i++){
+                        WikiCategory category = categories.get(i);
+                        categoryStr += Integer.toString(category.id) + ":" + category.title + ":" + category.titleTranslation;
+                        if(i != categories.size()-1) categoryStr += ", ";
+                    }
+                    line += "|" + categoryStr;
+                }
+                writer.println(line);
+            };
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/toolkits/Main.java
@@ -0,0 +1,31 @@
+package toolkits;
+
+/**
+ * Created by shaoshing on 2/6/14.
+ */
+
+import com.wikipedia.iub.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final String OUTPUT_FOLDER = "output";
+
+    static public void main(String[] args) throws IOException {
+        System.out.println("Input command:");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        String command = input.readLine().trim();
+
+        if(command.equals("hotpage")){
+            System.out.println("Input file path:");
+            String filePath = input.readLine().trim();
+            HotPagesToolkit.generateHotPagesTxt(filePath);
+        }
+
+
+    }
+}
